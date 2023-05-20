48a9590944137174f905c5209cdad90657fc7627
unknown <lakhanisujan.perfettosolutions@gmail.com>
2023-05-20 13:39:41 +0530

    react-folder-structure

---
+++ public/locales/gu/Gujarati.js
@@ -0,0 +1,1 @@
+console.log("Gujarati language");
---
+++ src/pages/about/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+    <h1>AboutPage</h1>
+    
+    </>
+  )
+}
+
+export default AboutPage
